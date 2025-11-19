b1565c1579cba44c41052c24acfcc4978c92a0f2
Islam Ahmed <softeng.islam@gmail.com>
2025-11-19 03:54:47 +0200

    Update

--- src/components/AddedOil.vue
+++ src/components/AddedOil.vue
@@ -21,14 +21,15 @@ export default {
 };
 </script>
 <template lang="pug">
-li( class="flex overflow-hidden items-center py-2 first-of-type:border-none border-t border-t-(--LTheme3) dark:border-t-(--Theme3) rounded-md p-2 bg-(--LTheme4) dark:bg-(--Theme4)")
+li( class="hover:shadow-2xl duration-200 flex overflow-hidden items-center py-2 first-of-type:border-none border-t border-t-(--LTheme3) dark:border-t-(--Theme3) rounded-md p-2 bg-(--LTheme4) dark:bg-(--Theme4)")
 	//- remove Oil
-	button(type="button" class="cursor-pointer flex items-center justify-center mr-2 w-7 h-7 rounded-sm hover:opacity-50 flex-none bg-(--LTheme3) dark:bg-(--Theme3)" @click="RemoveThisOil(oil)")
+	button(type="button" class="mr-2 w-7 h-7 cursor-pointer flex items-center justify-center rounded-sm hover:opacity-50 flex-none bg-(--LTheme3) dark:bg-(--Theme3)" @click="RemoveThisOil(oil)")
 		<svg class="w-5 fill-red-500" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M7 6V3C7 2.44772 7.44772 2 8 2H16C16.5523 2 17 2.44772 17 3V6H22V8H20V21C20 21.5523 19.5523 22 19 22H5C4.44772 22 4 21.5523 4 21V8H2V6H7ZM13.4142 13.9997L15.182 12.232L13.7678 10.8178L12 12.5855L10.2322 10.8178L8.81802 12.232L10.5858 13.9997L8.81802 15.7675L10.2322 17.1817L12 15.4139L13.7678 17.1817L15.182 15.7675L13.4142 13.9997ZM9 4V6H15V4H9Z"/></svg>
 	//- Oil Name
-	span(class="text-sm text-black dark:text-white pr-2") {{ oil.Name }}
-	input(class="text-green-500 ml-auto h-6 min-w-5 max-w-[50px] bg-(--LTheme3) dark:bg-(--Theme3) outline-none rounded-sm pl-1" type="number" maxlength="3" v-model="weightOfOil" @input="SetOilWeight(oil.Name)")
-	select(class="w-11 p-2 ml-2 outline-none rounded-md border bg-(--LTheme3) dark:bg-(--Theme3) border-gray-300 dark:border-gray-600 text-green-500" id="Units")
-		option(value="Gram") g
-		option(value="Kilo") K.g
+	span(class="pr-2 w-3/5 text-sm text-black dark:text-white overflow-hidden overflow-ellipsis whitespace-wrap") {{ oil.Name }}
+	div(class="overflow-hidden w-1/5 flex flex-col gap-1 items-center justify-center ml-auto")
+		select(class="w-full p-1 outline-none rounded-md border bg-(--LTheme3) dark:bg-(--Theme3) border-gray-300 dark:border-gray-600 text-green-500" id="Units")
+			option(value="Gram") g
+			option(value="Kilo") K.g
+		input(class="w-full p-1 text-black dark:text-white outline-none rounded-md border bg-(--LTheme3) dark:bg-(--Theme3)" type="number" maxlength="3" v-model="weightOfOil" @input="SetOilWeight(oil.Name)")
 </template>
--- src/components/TotalRecipe.vue
+++ src/components/TotalRecipe.vue
@@ -42,16 +42,16 @@ div(class="lyeWidget flex flex-col min-h-[200px] border border-transparent hover
 	ul(class="bg-(--LTheme3) dark:bg-(--Theme3) mt-4 p-2 pb-0 rounded-md h-full")
 		li(class="flex overflow-hidden py-2 first-of-type:border-none border-t border-t-(--LTheme2) dark:border-t-(--Theme2)")
 			span(class="text-sm text-black dark:text-white ") Water
-			span(class="text-blue-500 ml-auto duration-200") {{ AmountOfWater || 0}} #[span(class="text-gray-300") Gram]
+			span(class="text-blue-500 ml-auto duration-200") {{ AmountOfWater || 0}} #[span(class="text-black dark:text-white") Gram]
 		li(class="flex overflow-hidden py-2 first-of-type:border-none border-t border-t-(--LTheme2) dark:border-t-(--Theme2)")
 			span(class="text-sm text-black dark:text-white ") Lye - {{ OilStore.getTypeOfLye }}
-			span(class="text-blue-500 ml-auto duration-200") {{ AmountOfLye || 0 }} #[span(class="text-gray-300") Gram]
+			span(class="text-blue-500 ml-auto duration-200") {{ AmountOfLye || 0 }} #[span(class="text-black dark:text-white") Gram]
 		li(class="flex overflow-hidden py-2 border-t border-t-(--LTheme2) dark:border-t-(--Theme2)")
 			span(class="text-sm text-black dark:text-white ") Oils
-			span(class="text-blue-500 ml-auto") {{ AmountOfOils || 0}} #[span(class="text-gray-300") Gram]
+			span(class="text-blue-500 ml-auto") {{ AmountOfOils || 0}} #[span(class="text-black dark:text-white") Gram]
 		li(class="flex overflow-hidden py-2 border-t border-t-(--LTheme2) dark:border-t-(--Theme2)")
-			span(class="text-sm text-black dark:text-white ") Fragrance
-			span(class="text-blue-500 ml-auto") {{ AmountOfFragrance || 0 }} #[span(class="text-gray-300") Gram]
+			span(class="text-sm text-black dark:text-white") Fragrance
+			span(class="text-blue-500 ml-auto") {{ AmountOfFragrance || 0 }} #[span(class="text-black dark:text-white") Gram]
 	div(class="flex items-center overflow-hidden pt-4")
 		span(class="text-sm text-black dark:text-white") Recipe
 		span(class="text-blue-500 ml-auto bg-(--LTheme4) dark:bg-(--Theme4) rounded-full px-2 border border-(--LTheme3) dark:border-(--Theme3)") {{ TotalAmount || 0 }} #[span(class="text-black dark:text-white ") Gram]
